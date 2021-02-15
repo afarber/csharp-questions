93f43f5a68c9a573597a6d4296728aa85b72a755
Alexander Farber <Alexander.Farber@volkswagen-infotainment.com>
2021-02-15 15:19:30 +0100

    Add MatchStringsInTextFile project which reads "words" out of a small text file

---
+++ MatchStringsInTextFile/MatchStringsInTextFile/MatchStringsInTextFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MatchStringsInTextFile
+{
+    class Program
+    {
+        // match "words" consisting of 14 letters or digits
+        private static readonly Regex vinRegex = new Regex(@"\b[A-Z0-9]{14}\b");
+        private static readonly HashSet<string> set = new HashSet<string>();
+
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("TextFile1.txt"))
+            {
+                // read entire text file into the string
+                string resourceContents = reader.ReadToEnd();
+
+                // remove hash characters followed by anything
+                string commentsRemoved = Regex.Replace(resourceContents, @"#.*$", string.Empty, RegexOptions.Multiline);
+                Console.WriteLine("commentsRemoved: " + commentsRemoved);
+
+                // extract words and store them into the set
+                foreach (Match match in vinRegex.Matches(commentsRemoved))
+                {
+                    set.Add(match.Value.ToUpper());
+                }
+            }
+
+            foreach (string key in set)
+            {
+                Console.WriteLine("key: " + key);
+            }
+
+            // do not close the CMD console immediately
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# csharp-questions
-Test projects written in C#
+jetty-questions
+==========
+
+Small projects created on my way to Jetty nirvana
+
+The files are public domain - unless there is a different note (by their respective authors) on the top of some files.
+
+Screenshots
+==========
+
+[FcmAppContext](https://github.com/afarber/csharp-questions/tree/master/MatchStringsInTextFile):
+
+![screenshot](https://raw.github.com/afarber/csharp-questions/master/MatchStringsInTextFile/screenshot.png)
+
+
+![StackOverflow](http://stackoverflow.com/users/flair/165071.png)
+
+[My C# related questions at StackOverflow](http://stackoverflow.com/search?q=user:165071+[c#])
+
